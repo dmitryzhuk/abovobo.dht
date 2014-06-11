47d9a364fb6d98ae78dce6a3ac4bb7eae8857afd
Aleksey Yashchenko <tuxSlayer@gmail.com>
2014-06-12 00:45:26 +0300

    Some test helpers code

--- test/main/scala/org/abovobo/dht/DhtBuildingSmokeTest.scala
+++ test/main/scala/org/abovobo/dht/DhtBuildingSmokeTest.scala
@@ -52,69 +52,14 @@ object DhtBuildingSmokeTest extends App {
     "akka.actor.debug.unhandled" -> true))
     
     
-  def localEndpoint(ordinal: Int) = new InetSocketAddress(InetAddress.getLocalHost, 20000 + ordinal)
-
-//  case class NodeSystem(ordinal: Int, table: ActorRef, agent: ActorRef, controller: ActorRef, system: ActorSystem, storage: H2Storage) extends Disposable {
-//    val endpoint: InetSocketAddress = localEndpoint(ordinal)
-//    def dispose() {
-//        //system.shutdown()
-//        //system.awaitTermination()
-//        storage.close()     
-//    }
-//  }
-  
-
-  
-  class NodeSystem(ordinal: Int, routers: List[InetSocketAddress]) extends Actor {
-    
-    val h2 = H2Storage.open("~/db/dht-" + ordinal, true) 
-    val storage: Storage = h2
-    val reader: Reader = h2
-    val writer: Writer = h2
-    
-    val endpoint = localEndpoint(ordinal)
-    
-    def name(s: String) = s
-
-    val controller = this.context.actorOf(Controller.props(routers, reader, writer), name("controller"))
-
-    val agent = this.context.actorOf(Agent.props(endpoint, 10 seconds, controller), name("agent"))
-    
-    Thread.sleep(250) // Agent needs time to bind a socket and become an agent
-
-    val table = this.context.actorOf(Table.props(reader, writer, controller), name("table"))    
-    
-    override def receive() = {
-      case NodeSystem.Dispose => h2.close()
-      case NodeSystem.Describe => sender ! NodeSystem.NodeInfo(reader.id.get, endpoint, reader.nodes)
-      case msg => this.controller.forward(msg)
-    }
-  }
-  
-  object NodeSystem {
-    object Dispose
-    object Describe
-    case class NodeInfo(id: Integer160, endpoint: InetSocketAddress, nodes: Traversable[Node])
-  }
-  
 
   val system = ActorSystem("TestSystem", systemConfig)
-    
-  def createNode(ordinal: Int, routers: List[InetSocketAddress] = List()): ActorRef = {
-    system.actorOf(Props(classOf[NodeSystem], ordinal, routers), "Node-" + ordinal.toString)
-  }
+
   
   val timeoutDuration = 10 seconds
   implicit val timeout = Timeout(timeoutDuration)
 
-
-  val routerEndpoint = localEndpoint(0)
-  val router = createNode(0)
-
-  val nodes = List(router) ++ (for (i <- 1 to 30) yield {
-    Thread.sleep(100)
-    createNode(i, List(routerEndpoint))
-  })
+  val nodes = DhtNode.spawnNodes(system, 20000, 30)
   
   println("---------- waiting -------- ")
 
@@ -123,25 +68,25 @@ object DhtBuildingSmokeTest extends App {
 
   for (i <- 1 to 20) {
     println("---------- tables -------- ")
-    nodes.foreach { node =>
+    nodes.foreach { case (ep, node) =>
       
-      val nl = Await.result(node ? NodeSystem.Describe, timeoutDuration).asInstanceOf[NodeSystem.NodeInfo]
+      val info = Await.result(node ? DhtNode.Describe, timeoutDuration).asInstanceOf[DhtNode.NodeInfo]
       
-      println("dht table for: " + nl.id + "@" + nl.endpoint)
-      nl.nodes.foreach { entry => println("\t" + entry)}
+      println("dht table for: " + info.self.id + "@" + info.self.address)
+      info.nodes.foreach { entry => println("\t" + entry)}
     }    
 
     Thread.sleep(30 * 1000)
     
     println("---------- FIND NODE -------- ")
     
-    nodes.foreach { n =>
+    nodes.foreach { case (_, n) =>
       n ! Controller.FindNode(Integer160.random)
       Thread.sleep(2 * 1000)
     }
   }
 
-  nodes.foreach { n => n ! NodeSystem.Dispose }
+  nodes.foreach { case (_, n) => n ! DhtNode.Dispose }
 
   Thread.sleep(3 * 1000)
   
---
+++ test/main/scala/org/abovobo/dht/DhtNode.scala
@@ -0,0 +1,68 @@
+  package org.abovobo.dht
+
+  import akka.actor.Actor
+  import java.net.InetSocketAddress
+  import org.abovobo.dht.persistence.H2Storage
+  import org.abovobo.dht.persistence.Storage
+  import org.abovobo.dht.persistence.Reader
+  import org.abovobo.dht.persistence.Writer
+  import akka.actor.actorRef2Scala
+  import org.abovobo.integer.Integer160
+  import scala.concurrent.duration._
+  import akka.actor.ActorRef
+  import akka.actor.Props
+  import akka.actor.ActorSystem
+  import java.net.InetAddress
+
+class DhtNode(endpoint: InetSocketAddress, routers: List[InetSocketAddress]) extends Actor {
+  
+  val h2 = H2Storage.open("~/db/dht-" + self.path.name, true) 
+  val storage: Storage = h2
+  val reader: Reader = h2
+  val writer: Writer = h2
+    
+  val controller = this.context.actorOf(Controller.props(routers, reader, writer), "controller")
+
+  val agent = this.context.actorOf(Agent.props(endpoint, 10 seconds, controller), "agent")
+  
+  Thread.sleep(300) // Agent needs time to bind a socket and become an agent
+
+  val table = this.context.actorOf(Table.props(reader, writer, controller), "table")    
+  
+  Thread.sleep(100) // allow to generate id
+  
+  override def receive() = {
+    case DhtNode.Dispose => h2.close()
+    case DhtNode.Describe => sender ! DhtNode.NodeInfo(new Node(reader.id.get, endpoint), controller, reader.nodes)
+    case msg => this.controller.forward(msg)
+  }
+}
+
+object DhtNode {
+  object Dispose
+  object Describe
+  
+  case class NodeInfo(self: Node, controller: ActorRef, nodes: Traversable[Node])
+  
+  def props(endpoint: InetSocketAddress, routers: List[InetSocketAddress] = List()) = 
+    Props(classOf[DhtNode], endpoint, routers)
+
+    
+  def createNode(system: ActorSystem, endpoint: InetSocketAddress, routers: List[InetSocketAddress] = List()): ActorRef = {
+    system.actorOf(DhtNode.props(endpoint, routers), "Node-" + endpoint.getPort)
+  }    
+    
+  def spawnNodes(system: ActorSystem, portBase: Int, count: Int): Seq[(InetSocketAddress, ActorRef)] = {
+    
+    val routerEp = new InetSocketAddress(InetAddress.getLocalHost, portBase)
+        
+    val router = createNode(system, routerEp)
+    
+    val eps = for (i <- 1 until count) yield new InetSocketAddress(InetAddress.getLocalHost, portBase + i)
+
+    Seq(routerEp -> router) ++ eps.map { ep => 
+      Thread.sleep(1000)
+      ep -> createNode(system, ep, List(routerEp)) 
+    }
+  }
+}
