301c8d3846dc231267484a584f5d8454a713355e
Aleksey Yashchenko <tuxSlayer@gmail.com>
2014-06-11 16:13:02 +0300

    smoke test updated to host many Nodes on a single ActorSystem

--- test/main/scala/org/abovobo/dht/DhtBuildingSmokeTest.scala
+++ test/main/scala/org/abovobo/dht/DhtBuildingSmokeTest.scala
@@ -32,7 +32,12 @@ import akka.pattern.ask
 import akka.util.Timeout
 import scala.concurrent.Await
 import org.abovobo.dht.Controller.PutPlugin
+import akka.pattern.ask
 
+import scala.concurrent.Await
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
 
 
 object DhtBuildingSmokeTest extends App {
@@ -49,60 +54,98 @@ object DhtBuildingSmokeTest extends App {
     
   def localEndpoint(ordinal: Int) = new InetSocketAddress(InetAddress.getLocalHost, 20000 + ordinal)
 
-  case class NodeSystem(ordinal: Int, table: ActorRef, agent: ActorRef, controller: ActorRef, system: ActorSystem, storage: H2Storage) extends Disposable {
-    val endpoint: InetSocketAddress = localEndpoint(ordinal)
-    def dispose() {
-        system.shutdown()
-        system.awaitTermination()
-        storage.close()     
-    }
-  }
+//  case class NodeSystem(ordinal: Int, table: ActorRef, agent: ActorRef, controller: ActorRef, system: ActorSystem, storage: H2Storage) extends Disposable {
+//    val endpoint: InetSocketAddress = localEndpoint(ordinal)
+//    def dispose() {
+//        //system.shutdown()
+//        //system.awaitTermination()
+//        storage.close()     
+//    }
+//  }
   
-    
-  def createNode(ordinal: Int, routers: List[InetSocketAddress] = List()): NodeSystem = {
-      val h2 = H2Storage.open("~/db/dht-" + ordinal, true) 
 
+  
+  class NodeSystem(ordinal: Int, routers: List[InetSocketAddress]) extends Actor {
+    
+    val h2 = H2Storage.open("~/db/dht-" + ordinal, true) 
     val storage: Storage = h2
     val reader: Reader = h2
     val writer: Writer = h2
-      
-    val system = ActorSystem("TestSystem-" + ordinal, systemConfig)
+    
+    val endpoint = localEndpoint(ordinal)
+    
+    def name(s: String) = s
 
-    val controller = system.actorOf(Controller.props(routers, reader, writer), "controller")
+    val controller = this.context.actorOf(Controller.props(routers, reader, writer), name("controller"))
 
-    val agent = system.actorOf(Agent.props(localEndpoint(ordinal), 10 seconds, controller), "agent")
+    val agent = this.context.actorOf(Agent.props(endpoint, 10 seconds, controller), name("agent"))
     
     Thread.sleep(250) // Agent needs time to bind a socket and become an agent
 
-    val table = system.actorOf(Table.props(reader, writer, controller), "table")    
-  
-    NodeSystem(ordinal, table, agent, controller, system, h2)
+    val table = this.context.actorOf(Table.props(reader, writer, controller), name("table"))    
+    
+    override def receive() = {
+      case NodeSystem.Dispose => h2.close()
+      case NodeSystem.Describe => sender ! NodeSystem.NodeInfo(reader.id.get, endpoint, reader.nodes)
+      case msg => this.controller.forward(msg)
+    }
   }
+  
+  object NodeSystem {
+    object Dispose
+    object Describe
+    case class NodeInfo(id: Integer160, endpoint: InetSocketAddress, nodes: Traversable[Node])
+  }
+  
 
+  val system = ActorSystem("TestSystem", systemConfig)
+    
+  def createNode(ordinal: Int, routers: List[InetSocketAddress] = List()): ActorRef = {
+    system.actorOf(Props(classOf[NodeSystem], ordinal, routers), "Node-" + ordinal.toString)
+  }
+  
+  val timeoutDuration = 10 seconds
+  implicit val timeout = Timeout(timeoutDuration)
+
+
+  val routerEndpoint = localEndpoint(0)
   val router = createNode(0)
 
-  val nodes = List(router) ++ (for (i <- 1 to 3) yield {
-    Thread.sleep(3 * 1000)
-    createNode(i, List(router.endpoint))
+  val nodes = List(router) ++ (for (i <- 1 to 30) yield {
+    Thread.sleep(100)
+    createNode(i, List(routerEndpoint))
   })
   
   println("---------- waiting -------- ")
 
-  Thread.sleep(7 * 1000)
+  Thread.sleep(15 * 1000)
 
 
   for (i <- 1 to 20) {
     println("---------- tables -------- ")
     nodes.foreach { node =>
-      println("dht table for: " + node.storage.id + "@" + node.endpoint)
-      node.storage.nodes.foreach { entry => println("\t" + entry)}
+      
+      val nl = Await.result(node ? NodeSystem.Describe, timeoutDuration).asInstanceOf[NodeSystem.NodeInfo]
+      
+      println("dht table for: " + nl.id + "@" + nl.endpoint)
+      nl.nodes.foreach { entry => println("\t" + entry)}
     }    
 
-    Thread.sleep(60 * 1000)
+    Thread.sleep(30 * 1000)
+    
+    println("---------- FIND NODE -------- ")
+    
+    nodes.foreach { n =>
+      n ! Controller.FindNode(Integer160.random)
+      Thread.sleep(2 * 1000)
+    }
   }
-  
 
-  nodes.foreach(_.dispose)
+  nodes.foreach { n => n ! NodeSystem.Dispose }
+
+  Thread.sleep(3 * 1000)
   
-  Thread.sleep(5 * 1000)
+  system.shutdown()
+  
+  Thread.sleep(1 * 1000)
 }
