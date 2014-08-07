c8d0215c2a3e78d384edfb651433490a6ccc48ee
Dmitry Zhuk <dmitryzhuk@gmail.com>
2014-08-07 15:09:01 +0300

    Test can now be run in separate JVMs simultaneously using JVM arguments.

--- test/main/scala/org/abovobo/dht/DhtTest.scala
+++ test/main/scala/org/abovobo/dht/DhtTest.scala
@@ -34,14 +34,24 @@ class DHTTest(system: ActorSystem)
 
   def this() = this(ActorSystem("DHTTest", ConfigFactory.parseString("akka.loglevel=debug")))
 
-  val routerEndpoint = new InetSocketAddress(InetAddress.getLoopbackAddress, 30000)
-  val routerDS = {
-    using(new InputStreamReader(this.getClass.getResourceAsStream("/tables.sql"))) { reader =>
-      DataSource("jdbc:h2:~/db/dht-router", reader).close()
+  val startingPort = this.system.settings.config.getInt("port")
+  val startingIndex = this.system.settings.config.getInt("index")
 
-    }
-    DataSource("jdbc:h2:~/db/dht-router;SCHEMA=ipv4")
-  }
+  val routerEndpoint =
+    if (this.system.settings.config.hasPath("router"))
+      new InetSocketAddress(InetAddress.getLoopbackAddress, this.system.settings.config.getInt("router"))
+    else
+      new InetSocketAddress(InetAddress.getLoopbackAddress, this.startingPort)
+
+  val routerDS =
+    if (this.system.settings.config.hasPath("router")) {
+      using(new InputStreamReader(this.getClass.getResourceAsStream("/tables.sql"))) { reader =>
+        DataSource("jdbc:h2:~/db/dht-router", reader).close()
+
+      }
+      DataSource("jdbc:h2:~/db/dht-router;SCHEMA=ipv4")
+    } else
+      null
 
   def makeDS(id: Long) = {
     using(new InputStreamReader(this.getClass.getResourceAsStream("/tables.sql"))) { reader =>
@@ -51,25 +61,27 @@ class DHTTest(system: ActorSystem)
     DataSource("jdbc:h2:~/db/dht-node" + id + ";SCHEMA=ipv4")
   }
   val nodes: ListBuffer[(ActorRef, DataSource)] = new ListBuffer()
-  var index = 1
-  val base = 30000
+  var index = this.startingIndex
+  val base = this.routerEndpoint.getPort
 
   override def beforeAll() = {
 
   }
 
   override def afterAll() = {
-    Thread.sleep(10000)
+    println("AFTER ALL")
+    Thread.sleep(300000)
     TestKit.shutdownActorSystem(this.system)
   }
 
-  "Router node" when {
-
-    "just created" must {
-      "log FindNode failure" in {
-        val router = system.actorOf(
-          Props(classOf[Node], this.routerDS, this.routerEndpoint, List.empty[InetSocketAddress], 0L),
-          "router")
+  if (this.system.settings.config.hasPath("router")) {
+    "Router node" when {
+      "just created" must {
+        "log FindNode failure" in {
+          val router = system.actorOf(
+            Props(classOf[Node], this.routerDS, this.routerEndpoint, List.empty[InetSocketAddress], 0L),
+            "router")
+        }
       }
     }
   }
@@ -77,16 +89,19 @@ class DHTTest(system: ActorSystem)
   "Normal node" when {
     "just created" must {
       "start FIND_NODE using router" in {
-        val ds = makeDS(index)
-        val node = system.actorOf(
-          Props(classOf[Node],
-            ds,
-            new InetSocketAddress(InetAddress.getLoopbackAddress, this.base + index),
-            List(this.routerEndpoint),
-            index.toLong),
-          "node" + index)
-        nodes += (node -> ds)
-        index += 1
+        for (i <- 0 until 1000) {
+          val ds = makeDS(index)
+          val node = system.actorOf(
+            Props(classOf[Node],
+              ds,
+              new InetSocketAddress(InetAddress.getLoopbackAddress, this.base + index),
+              List(this.routerEndpoint),
+              index.toLong),
+            "node" + index)
+          nodes += (node -> ds)
+          index += 1
+          Thread.sleep(1)
+        }
       }
     }
   }
