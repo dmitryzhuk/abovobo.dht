c89780cb211b3e39ae6b77ab5114426380cecefe
Dmitry Zhuk <dmitryzhuk@gmail.com>
2014-08-19 13:44:33 +0300

    Added utility methods

--- src/main/scala/org/abovobo/dht/Bucket.scala
+++ src/main/scala/org/abovobo/dht/Bucket.scala
@@ -27,5 +27,23 @@ class Bucket(val start: Integer160, val end: Integer160, val seen: java.util.Dat
    * @param id An id to test.
    * @return   True if given id is within the range of the bucket.
    */
-  def in(id: Integer160): Boolean = this.start <= id && id <= this.end
+  def @:(id: Integer160): Boolean = this.start <= id && id <= this.end
+
+  /**
+   * Returns the range size of this bucket.
+   * @return the range size of this bucket.
+   */
+  def length: Integer160 = this.end - this.start
+
+  /**
+   * Returns random value which lies within the range of this bucket.
+   * @return random value which lies within the range of this bucket.
+   */
+  def random: Integer160 = this.start + Integer160.random % (this.length + 1)
+
+  /**
+   * Returns middle point of this bucket length.
+   * @return middle point of this bucket length.
+   */
+  def mid: Integer160 = this.start + (this.length >> 1)
 }
